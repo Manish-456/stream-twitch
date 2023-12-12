5c4c5157e27dc9afe423547373df0e84f25464e0
Manish <tamangmanish3456@gmail.com>
2023-12-12 15:43:15 +0545

    added logo and theme

---
+++ app/(auth)/_components/logo.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image"
+import { Poppins } from "next/font/google"
+
+import { cn } from "@/lib/utils"
+
+ const font = Poppins({
+    weight: ["200", "300", "400", "500", "600", "700", "800"],
+    subsets : ["latin"]
+})
+
+export function Logo() {
+  return (
+    <div className="flex flex-col items-center gap-y-4">
+        <div className="bg-white p-1 rounded-full">
+            <Image
+            src={'/spooky.svg'}
+            alt="streamhub"
+            width={80}
+            height={80}
+            />
+        </div>
+    <div className={cn("flex flex-col items-center", font.className)}>
+        <p className="text-xl font-semibold">Streamhub</p>
+        <p className="text-sm text-muted-foreground">Let&apos;s play</p>
+    </div>
+    </div>
+  )
+}
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Logo } from './_components/logo'
 
 export default function AuthLayout({children}:{
     children: React.ReactNode
 }) {
   return (
-    <div className='h-full flex items-center justify-center'>
+    <div className='h-full flex-col space-y-6 flex items-center justify-center'>
+        <Logo />
       {children}
     </div>
   )
