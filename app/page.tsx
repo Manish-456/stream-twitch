a1f8ff8890c56153f391cbfe61815d00a5b4beec
Manish <tamangmanish3456@gmail.com>
2023-12-12 14:19:41 +0545

    feat: added clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <>
-    <h1 className="text-red-500 font-extrabold text-4xl text-center">Hello World!</h1>
+    <h1 className="text-red-500 font-extrabold text-4xl text-center">
+      <UserButton afterSignOutUrl="/" />
+    </h1>
     </>
   )
 }
