20c11d952c34cd401f4149a855babb7f573440aa
Manish <tamangmanish3456@gmail.com>
2023-12-13 15:29:32 +0545

    added get-recommended-users service

---
+++ lib/recommended-service.ts
@@ -0,0 +1,12 @@
+import { db } from "@/lib/db";
+import { getSelf } from "@/lib/auth-service";
+
+export async function getRecommendedUsers(){
+    const users = await db.user.findMany({
+        orderBy : {
+            createdAt : "desc"
+        }
+    });
+
+    return users;
+}
