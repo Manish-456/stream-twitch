e08c1aa33754a60c33061a20365c2d863d2eafd3
Manish <tamangmanish3456@gmail.com>
2023-12-13 15:17:03 +0545

    added collapsible sidebar

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import { Navbar } from './_components/navbar'
+import React from "react";
 
-export default function BrowseLayout({children} : {
-    children : React.ReactNode
+import { Navbar } from "./_components/navbar";
+import { Sidebar } from "./_components/sidebar";
+import { Container } from "./_components/container";
+
+export default function BrowseLayout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
   return (
     <>
-    <Navbar />
-    <div className='flex h-full pt-20'>
-     {children}    
-    </div>
+      <Navbar />
+      <div className="flex h-full pt-20">
+        <Sidebar />
+        <Container>
+        {children}
+        </Container>
+      </div>
     </>
-  )
+  );
 }
---
+++ components/hint.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+import { useEffect, useState } from "react";
+
+interface HintProps {
+  label: string;
+  children: React.ReactNode;
+  asChild?: boolean;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "end" | "center";
+}
+
+export function Hint({ label, children, asChild, side, align }: HintProps) {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, []);
+
+    if(!isMounted) return null;
+    
+  return (
+    <TooltipProvider>
+      <Tooltip delayDuration={0}>
+        <TooltipTrigger asChild={asChild}>{children}</TooltipTrigger>
+        <TooltipContent
+          className="bg-white text-black"
+          side={side}
+          align={align}
+        >
+          <p className="font-semibold">{label}</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
