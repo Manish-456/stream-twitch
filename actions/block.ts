a22e684e127acf9e451784afec9d276c8cda75a2
Manish <tamangmanish3456@gmail.com>
2023-12-14 15:26:04 +0545

    added block/unblock user actions

---
+++ actions/block.ts
@@ -0,0 +1,31 @@
+import { revalidatePath } from "next/cache";
+import { blockUser, unBlockUser } from "@/lib/block-service";
+
+export async function onBlock(id : string){
+    // TODO : Adapt to disconnect from livestream
+    // TODO : Allow ability to kick the guest
+    
+    const blockedUser = await blockUser(id);
+    
+    revalidatePath("/")
+
+    if(blockedUser){
+        revalidatePath(`/${blockedUser.blocked.username}`)
+    }
+
+    return blockedUser;
+
+}
+
+export async function onUnblock(id : string){
+    const unblockedUser = await unBlockUser(id);
+
+    revalidatePath("/")
+
+    if(unblockedUser){
+        revalidatePath(`/${unblockedUser.blocked.username}`)
+    }
+
+    return unblockedUser;
+
+}
---
+++ lib/block-service.ts
@@ -0,0 +1,103 @@
+import { db } from '@/lib/db';
+import { getSelf } from '@/lib/auth-service';
+
+export async function isBlockedByUser(id : string){
+    try {
+        const self = await getSelf();
+
+        const otherUser = await db.user.findUnique({
+            where : {
+                id
+            }
+        });
+
+        if(!otherUser) throw new Error('User not found');
+
+        if(self.id === otherUser.id){
+            return false;
+        }
+
+        const existingBlock = await db.block.findUnique({
+            where : {
+            blockedId_blockerId: {
+                blockedId : self.id,
+                blockerId : otherUser.id
+            }    
+            }
+        })
+
+        return !!existingBlock;
+    } catch (error) {
+        return false;
+    }
+}
+
+
+export async function blockUser(id : string){
+    const self = await getSelf();
+
+    const otherUser = await db.user.findUnique({
+        where : {
+            id
+        }
+    });
+
+    if(!otherUser) throw new Error("User not found");
+
+    if(self.id === otherUser.id) throw new Error("Cannot block yourself");
+
+    const existingBlock = await db.block.findUnique({
+        where : {
+            blockedId_blockerId : {
+                blockedId : otherUser.id,
+                blockerId : self.id
+            }
+        }
+    });
+
+    if(existingBlock) throw new Error("Already blocked");
+
+    const block = await db.block.create({
+        data : {
+            blockedId : otherUser.id,
+            blockerId : self.id
+        },
+        include : {
+            blocked : true
+        }
+    });
+
+    return block;
+}
+
+export async function unBlockUser(id : string){
+    const self = await getSelf();
+
+    const otherUser = await db.user.findUnique({
+        where : {
+            id
+        }
+    });
+
+    if(!otherUser) throw new Error("User not found");
+
+    if(self.id === otherUser.id) throw new Error("Cannot block yourself");
+
+    const existingBlock = await db.block.findUnique({
+        where : {
+            blockedId_blockerId : {
+                blockedId : otherUser.id,
+                blockerId : self.id
+            }
+        }
+    });
+
+    if(!existingBlock) throw new Error("Not blocked");
+
+    const unblock = await db.block.delete({
+        where : {id : existingBlock.id},
+        include : {blocked : true}
+    })
+
+    return unblock;
+}
